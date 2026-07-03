ce1ffad0b232f9d762a04031dacc84d4c4683850
Nino-FR-04 <davidfrx322@gmail.com>
2026-07-03 03:12:13 -0500

    Correcciones en el movimiento del personaje, ademas de la implementacion de una escena de prueba

--- scripts/playerScripts/player.gd
+++ scripts/playerScripts/player.gd
@@ -1,1 +1,23 @@
 extends CharacterBody2D
+
+var controller := PlayerController.new()
+@onready var animSprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var direction := Input.get_axis("left", "right")
+	print("Direction:", direction)
+	controller.move(direction)
+	velocity = controller.velocity
+
+	move_and_slide()
+	self.update_anim(direction)
+	print("Posición:", global_position)
+	
+func update_anim(direction: float):
+	if direction != 0:
+		self.animSprite.flip_h = direction < 0
+		self.animSprite.play("run_anim")
+	else:
+		self.animSprite.play("idle_anim")
+		
+		
--- scripts/playerScripts/player_controller.gd
+++ scripts/playerScripts/player_controller.gd
@@ -1,9 +1,10 @@
 class_name PlayerController
 extends RefCounted
 
-var speed: float = 200.0
+var speed: float = 200
 var velocity := Vector2.ZERO
 
 func move(direction: float):
 	direction = clamp(direction, -1.0, 1.0)
 	velocity.x = direction * speed
+	print(velocity)
