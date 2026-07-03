be3818ff28b824dc0dec799906ddaa072611946c
Nino-FR-04 <davidfrx322@gmail.com>
2026-07-03 08:16:46 -0500

    Tests: Diamante como premio al recoger todas las monedas del mapa

---
+++ tests/test_diamond.gd
@@ -0,0 +1,60 @@
+extends GutTest
+
+var player
+var diamond
+
+func before_each():
+	player = PlayerController.new()
+	diamond = DiamondController.new()
+
+# El diamante permanece bloqueado
+func test_diamondIsLoocked():
+	assert_false(diamond.is_unlocked)
+	
+# El diamante requiere 5 monedas por defecto
+func test_diamondDefaultRequiredCoins():
+	assert_eq(diamond.required_coins, 5)
+
+# El diamante no se desbloquea sin monedas
+func test_diamondDoesNotUnlockedWithoutCoins():
+
+	player.coin_count = 0
+
+	var result = diamond.try_open(player.coin_count)
+
+	assert_false(result)
+	assert_false(diamond.is_unlocked)
+
+# El diamante no se desbloquea con menos monedas de las necesarias
+func test_diamondDoesNotUnlockedWithInsufficientCoins():
+
+	diamond.required_coins = 5
+
+	player.coin_count = 3
+
+	var result = diamond.try_open(player.coin_count)
+
+	assert_false(result)
+	assert_false(diamond.is_open)
+
+# El diamante se desbloquea con la cantidad exacta
+func test_diamondUnlockedWithExactRequiredCoins():
+
+	diamond.required_coins = 5
+
+	player.coin_count = 5
+
+	var result = diamond.try_open(player.coin_count)
+
+	assert_true(result)
+	assert_true(diamond.is_open)
+
+# Cambiar la cantidad de monedas requeridas
+func test_diamondWithDifferentRequiredAmount():
+
+	diamond.required_coins = 15
+	player.coin_count = 10
+	assert_false(diamond.try_open(player.coin_count))
+
+	player.coin_count = 15
+	assert_true(diamond.try_open(player.coin_count))
